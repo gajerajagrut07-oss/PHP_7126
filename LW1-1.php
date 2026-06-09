bd28bc5f63e7dc428385aacdbf92bba8000a2dd7
gajerajagrut07-oss <gajerajagrut07@gmail.com>
2026-06-09 10:43:52 +0530

    Add files via upload

---
+++ LW1-1.php
@@ -0,0 +1,14 @@
+<?php
+
+define("COLLEGE_NAME", "Marwadi University");
+
+$name = "Jagrut";
+$semester = "4";
+$cgpa = 8.31;
+
+echo "<h3>Previous Semester Result</h3>";
+echo "College Name : " . COLLEGE_NAME . "<br>";
+echo "Student Name : " . $name . "<br>";
+echo "Semester : " . $semester . "<br>";
+echo "CGPA : " . $cgpa . "<br>";
+?>
