62122b970a6f3196b2d9e2dc2bcb390fda943724
gajerajagrut07-oss <gajerajagrut07@gmail.com>
2026-06-10 09:51:14 +0530

    Add files via upload

---
+++ LW1-2.php
@@ -0,0 +1,12 @@
+<?php
+$a = 25;
+$b = 40;
+
+if ($a > $b) {
+    echo "Maximum Number = " . $a . "<br>";
+    echo "Minimum Number = " . $b;
+} else {
+    echo "Maximum Number = " . $b . "<br>";
+    echo "Minimum Number = " . $a;
+}
+?>
