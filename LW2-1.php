03f4c2a78966865e92236e616a03e096dbb4b06b
gajerajagrut07-oss <gajerajagrut07@gmail.com>
2026-07-28 08:06:48 +0530

    Add files via upload

---
+++ LW2-1.php
@@ -0,0 +1,10 @@
+<?php
+
+function collegeInfo() {
+    echo "College Name: Marwadi University<br>";
+    echo "Course Name: BCA<br>";
+    echo "Academic Year: 2024-2027<br>";
+}
+
+collegeInfo();
+?>
---
+++ LW2-2.php
@@ -0,0 +1,10 @@
+<?php
+
+function studentDetails($name, $enrollment, $semester) {
+    echo "Student Name : $name<br>";
+    echo "Enrollment Number : $enrollment<br>";
+    echo "Semester : $semester<br>";
+}
+
+studentDetails("Jagrut Gajera", "92400527126", 5);
+?>
