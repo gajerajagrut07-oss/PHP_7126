86d235b51f3f14c005da9a881eaced540ecd1194
Gajera Jagrut <gajerajagrut07@gmail.com>
2026-08-05 07:53:25 +0530

    Add files via upload

---
+++ session_1.php
@@ -0,0 +1,16 @@
+<?php
+
+//Starting the session
+session_start();
+
+?>
+
+<html>
+    <body>
+        <?php
+        
+        $_SESSION['username'] = "Jagrut";
+        
+        ?>
+    </body>
+</html>
