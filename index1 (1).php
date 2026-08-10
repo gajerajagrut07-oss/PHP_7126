f9dd3606a704b9b5b2613ed9260b4fc6730eddd5
Gajera Jagrut <gajerajagrut07@gmail.com>
2026-08-10 17:52:57 +0530

    Add files via upload

---
+++ index1 (1).php
@@ -0,0 +1,17 @@
+<?php
+
+    // array = "variable" whivh can more than one value at a time
+    $foods = array("apple", "orange", "banana", "coconut");
+
+    //$foods[0] = "pineapple";
+    //array_push($foods, "pineapple");
+    //array_pop($foods);
+    //array_shift($foods);
+    //$foods = array_reverse($foods);
+    //echo count($foods);
+
+    foreach($foods as $food) {
+        echo $food . "<br>";
+    }
+
+?>
