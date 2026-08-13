fce3c5c57b724f4d00fbbafe3b408b5de0ed0b46
Gajera Jagrut <gajerajagrut07@gmail.com>
2026-08-13 06:39:17 +0530

    Add files via upload

---
+++ 1.1.php
@@ -0,0 +1,14 @@
+<?php
+define("COLLEGE", "Marwadi University");
+
+$name = "Jagrut";
+$semester = "Semester 2";
+$result = "Pass";
+$cgpa = 7.71;
+
+echo "College: " . COLLEGE . "<br>";
+echo "Name: $name<br>";
+echo "Semester: $semester<br>";
+echo "Result: $result<br>";
+echo "CGPA: $cgpa";
+?>
---
+++ 1.2.php
@@ -0,0 +1,12 @@
+<?php
+$a = 10;
+$b = 25;
+
+if ($a > $b) {
+    echo "Maximum: $a<br>";
+    echo "Minimum: $b";
+} else {
+    echo "Maximum: $b<br>";
+    echo "Minimum: $a";
+}
+?>
